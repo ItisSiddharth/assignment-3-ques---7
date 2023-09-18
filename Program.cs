3763ebef39686340a138fe83d37c0c4ff7a507af
sidharth <sidharthkumar@gmail.com>
2023-09-18 15:03:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User l1 = new User();
+            l1.setName("Sidharth");
+            Console.WriteLine(l1.getName());
+            Console.ReadLine();
+        }
+    }
+    class User
+    {
+        public String name = "SAT";
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+        public String getName()
+        {
+            return this.name;
+        }
+    }
+
+}
